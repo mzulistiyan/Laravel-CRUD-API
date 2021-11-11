bb15d8dd5b9c863ee070b1c0d5320c2b06c14379
mzulistiyan <izulistiyan@gmail,com>
2021-11-11 22:28:42 +0700

    first API Laravel

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transactions;
+use Illuminate\Http\Request;
+use Midtrans\Config;
+use Midtrans\Snap;
+use Midtrans\Notification;
+
+class MidtransController extends Controller
+{
+    public function callback(Request $request)
+    {
+        // Set konfigurasi midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        // Buat instance midtrans notification
+        $notification = new Notification();
+
+        // Assign ke variable untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+        // Cari transaksi berdasarkan ID
+        $transaction = Transactions::findOrFail($order_id);
+
+        // Handle notification status midtrans
+        if ($status == 'capture') {
+            if ($type == 'credit_card'){
+                if($fraud == 'challenge'){
+                    $transaction->status = 'PENDING';
+                }
+                else {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        }
+        else if ($status == 'settlement'){
+            $transaction->status = 'SUCCESS';
+        }
+        else if($status == 'pending'){
+            $transaction->status = 'PENDING';
+        }
+        else if ($status == 'deny') {
+            $transaction->status = 'CANCELLED';
+        }
+        else if ($status == 'expire') {
+            $transaction->status = 'CANCELLED';
+        }
+        else if ($status == 'cancel') {
+            $transaction->status = 'CANCELLED';
+        }
+
+        // Simpan transaksi
+        $transaction->save();
+
+        
+        
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function success(Request $request)
+    {
+        return view('midtrans.success');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function unfinish(Request $request)
+    {
+        return view('midtrans.unfinish');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function error(Request $request)
+    {
+        return view('midtrans.error');
+    }
+
+}
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,60 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('User') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mb-10">
+                <a href="{{ route('users.create') }}" class="">
+                    + Create User
+                </a>
+            </div>
+            <div class="bg-white">
+                <table class="table-auto w-full">
+                    <thead>
+                    <tr>
+                        <th class="border px-6 py-4">ID</th>
+                        <th class="border px-6 py-4">Name</th>
+                        <th class="border px-6 py-4">Email</th>
+                        <th class="border px-6 py-4">Roles</th>
+                        <th class="border px-6 py-4">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($user as $item)
+                            <tr>
+                                <td class="border px-6 py-4">{{ $item->id }}</td>
+                                <td class="border px-6 py-4 ">{{ $item->name }}</td>
+                                <td class="border px-6 py-4">{{ $item->email }}</td>
+                                <td class="border px-6 py-4">{{ $item->roles }}</td>
+                                <td class="border px-6 py- text-center">
+                                    <a href="{{ route('users.edit', $item->id) }}" class="">
+                                        Edit
+                                    </a>
+                                    <form action="{{ route('users.destroy', $item->id) }}" method="POST" class="inline-block">
+                                        {!! method_field('delete') . csrf_field() !!}
+                                        <button type="submit" class="">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                               <td colspan="6" class="border text-center p-5">
+                                   Data Tidak Ditemukan
+                               </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+            <div class="text-center mt-5">
+                {{ $user->links() }}
+            </div>
+        </div>
+    </div>
+</x-app-layout>
